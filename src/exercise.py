99b10b68e84c8221ad84f2374f9aa2f04ca19b8b
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 20:21:49 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    strList = []
+    while(True):
+      str = input()
+      if(str==""):
+        break
+      strList.append(str)
+    print("In total: %s"%len(strList))
 
 if __name__ == '__main__':
     main()
